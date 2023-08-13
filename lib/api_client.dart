e58023f0a0985306656d19d9b4cf02686d1e8ff7
ChanakaDev <mr.mbcnb@gmail.com>
2023-08-13 14:43:26 +0530

    Completed data fetching and showing in a listview

--- lib/api_client.dart
+++ lib/api_client.dart
@@ -1,18 +1,19 @@
 // ignore_for_file: avoid_print
 
+import 'package:api_calling/post_model.dart';
 import 'package:http/http.dart' as http;
 
 class APIClient {
-  void fetchData() async {
-    var request = http.Request(
-        'GET', Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+  Future<List<Posts>?> fetchData() async {
+    var client = http.Client();
 
-    http.StreamedResponse response = await request.send();
+    var uri = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    var response = await client.get(uri);
 
     if (response.statusCode == 200) {
-      print(await response.stream.bytesToString());
-    } else {
-      print(response.reasonPhrase);
+      var json = response.body;
+      return postsFromJson(json);
     }
+    return null;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
+// ignore_for_file: avoid_print
+
 import 'package:api_calling/api_client.dart';
+import 'package:api_calling/post_model.dart';
 import 'package:flutter/material.dart';
+
+// Craete an instance of the class APIClient
+APIClient apiClient = APIClient();
+List<Posts>? allPosts;
 
 void main() {
   runApp(const MyApp());
@@ -33,26 +40,42 @@ class _PostsScreenState extends State<PostsScreen> {
   void initState() {
     super.initState();
 
-    // Craete an instance of the class APIClient
-    APIClient apiClient = APIClient();
-    // Call the fetchData method
-    apiClient.fetchData();
+    getData();
+  }
+
+  getData() async {
+    allPosts = await apiClient.fetchData();
+
+    print(allPosts);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text(
-            "API Calling",
-            style: TextStyle(
-              color: Colors.white,
-            ),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text(
+          "API Calling",
+          style: TextStyle(
+            color: Colors.white,
           ),
-          centerTitle: true,
         ),
-        // ListView builder
-        body: ListView());
+        centerTitle: true,
+      ),
+      // ListView builder
+      body: ListView.builder(
+        itemCount: allPosts!.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              ListTile(
+                title: Text(allPosts![index].title),
+              ),
+              const Divider()
+            ],
+          );
+        },
+      ),
+    );
   }
 }
